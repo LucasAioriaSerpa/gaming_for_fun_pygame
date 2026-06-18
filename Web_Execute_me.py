20cc8e45fd5da287a934da18a3ca67cdac0a7323
Lucas Aioria Serpa <Lucasaioriaserpa@outlook.com>
2026-06-17 21:20:57 -0300

    fast-fix

--- Web_Execute_me.py
+++ Web_Execute_me.py
@@ -1,9 +1,9 @@
 
 import asyncio
-
 import exe
 
 try:
     asyncio.get_running_loop()
     asyncio.create_task(exe.Launcher.run())
-except RuntimeError: asyncio.run(exe.Launcher.run())
+    asyncio.ensure_future(exe.Launcher.run())
+except RuntimeError as E: asyncio.run(exe.Launcher.run())
